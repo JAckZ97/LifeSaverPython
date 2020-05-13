d1f4bea2c1f4ca4806e4aa62c7942b59cf5384c0
Jack Zhang <zzj1353832196@gmail.com>
2020-05-13 17:16:30 -0400

    Add font module

---
+++ Check_the_day/font.py
@@ -0,0 +1,12 @@
+import pyfiglet
+
+# result = pyfiglet.figlet_format("Document", font="5lineoblique")
+# print(result)
+
+# result1 = pyfiglet.figlet_format("Document", font="doh")
+# print(result1)
+
+# result3 = pyfiglet.figlet_format("Stop now")
+print(pyfiglet.figlet_format("Sorry I plan"))
+print(pyfiglet.figlet_format("to stop the"))
+print(pyfiglet.figlet_format("challenge now"))
